1123b25f28602a894bee4998462a4c35e7943fe0
k4th4r1 <132467227+k4th4r1@users.noreply.github.com>
2023-05-03 10:41:26 -0300

    Add files via upload

---
+++ Exercicio5.java
@@ -0,0 +1,19 @@
+package Atividades;
+
+import java.util.Scanner;
+
+public class Exercicio5 {
+    public static  void main (String []args) {
+    	Scanner s = new Scanner(System.in);
+    	double numero1, numero2;
+    	System.out.println("Entre com um número");
+    	numero1 = s.nextDouble();
+    	
+    	System .out.println("Entre com um número");
+    	numero2 = s.nextDouble();
+    	
+    	System.out.println(numero1/numero2);
+    	
+    }
+	
+}
